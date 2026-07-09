84ed206f8d31dff22ba1eb4fdc42a98a14160b13
eduardofierroduque <eduardofierroduque@gmail.com>
2026-07-09 11:32:15 -0400

    feat: orange accent on Blend Mode dropdown
    Hover border: kMutedColor -> kAccentColor (#FF4800)
    Dropdown selected: added selection-color white text
    Selection background already was kAccentColor.

--- src/ui_v2/layer_panel_v2.cpp
+++ src/ui_v2/layer_panel_v2.cpp
@@ -159,8 +159,8 @@ LayerPanelV2::LayerPanelV2(std::shared_ptr<AppState> state, QWidget* parent)
         QString("QComboBox{background:%1;color:%2;border:1px solid %3;border-radius:3px;padding:3px 6px;font-size:11px;}")
             .arg(kInputBg, kTextColor, kBorderColor) +
         QString("QComboBox:hover{border-color:%1;}")
-            .arg(kMutedColor) +
-        QString("QComboBox QAbstractItemView{background:%1;color:%2;selection-background-color:%3;border:1px solid %4;}")
+            .arg(kAccentColor) +
+        QString("QComboBox QAbstractItemView{background:%1;color:%2;selection-background-color:%3;selection-color:#fff;border:1px solid %4;}")
             .arg(kInputBg, kTextColor, kAccentColor, kBorderColor));
     QObject::connect(blendCombo_, QOverload<int>::of(&QComboBox::currentIndexChanged),
                      this, &LayerPanelV2::onBlendModeChanged);
